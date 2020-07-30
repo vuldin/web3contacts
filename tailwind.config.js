6af66713ca23bc88509114ecda49b0be30f92b64
Joshua Purcell <hello@vuld.in>
2020-07-30 09:22:21 -0400

    basic tailwind integration

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        contacts: "30px auto",
+        skills: "100px auto",
+        "md-main": "min-content auto",
+      },
+      gridTemplateRows: {
+        "md-main": "1fr auto",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
