13270e7b8bbef1d62d6b3a42970285950cbecd45
Joshua Purcell <me@vuld.in>
2020-08-04 10:39:28 -0400

    move Connect from nav bar to Welcome page

--- src/App.js
+++ src/App.js
@@ -93,86 +93,86 @@ export default function App() {
                       </div>
                     </div>
                   </div>
-                  <div className="hidden md:block">
-                    <div className="flex items-center ml-4 md:ml-6">
-                      {!isConnected ? (
-                        <Connect />
-                      ) : (
-                        <>
-                          <span className="inline-block text-sm text-gray-600">
-                            {showSync && (isBoxSyncing ? `${DESYNCED_TEXT}` : `${SYNCED_TEXT}`)}
-                          </span>
-                          <button
-                            className="p-1 text-gray-400 border-2 border-transparent rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
-                            aria-label="Notifications"
-                          >
-                            <Bell className="w-6 h-6" />
-                          </button>
-
-                          <div className="relative ml-3">
-                            <div>
-                              <button
-                                className="flex items-center max-w-xs text-sm text-white rounded-full focus:outline-none focus:shadow-solid"
-                                id="user-menu"
-                                aria-label="User menu"
-                                aria-haspopup="true"
-                                onClick={() => setIsOpen(!isOpen)}
-                              >
-                                {profile?.image?.[0]?.contentUrl?.['/'] && (
-                                  <img
-                                    className="inline-block w-8 h-8 rounded"
-                                    src={`https://ipfs.infura.io/ipfs/${profile.image[0].contentUrl['/']}`}
-                                    alt="profile"
-                                  />
-                                )}
-                              </button>
-                            </div>
-                            <Transition
-                              show={isOpen}
-                              enter="transition ease-out duration-100 transform"
-                              enterFrom="opacity-0 scale-95"
-                              enterTo="opacity-100 scale-100"
-                              leave="transition ease-in duration-75 transform"
-                              leaveFrom="opacity-100 scale-100"
-                              leaveTo="opacity-0 scale-95"
+                  {isConnected && (
+                    <>
+                      <div className="hidden md:block">
+                        <div className="flex items-center ml-4 md:ml-6">
+                          <>
+                            <span className="inline-block text-sm text-gray-600">
+                              {showSync && (isBoxSyncing ? `${DESYNCED_TEXT}` : `${SYNCED_TEXT}`)}
+                            </span>
+                            <button
+                              className="p-1 text-gray-400 border-2 border-transparent rounded-full hover:text-white focus:outline-none focus:text-white focus:bg-gray-700"
+                              aria-label="Notifications"
                             >
-                              <div className="absolute right-0 w-48 mt-2 origin-top-right rounded-md shadow-lg">
-                                <div className="py-1 bg-white rounded-md shadow-xs">
-                                  <a
-                                    href="#"
-                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                  >
-                                    Settings
-                                  </a>
-                                  <a
-                                    href="#"
-                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                  >
-                                    Sign out
-                                  </a>
-                                </div>
+                              <Bell className="w-6 h-6" />
+                            </button>
+
+                            <div className="relative ml-3">
+                              <div>
+                                <button
+                                  className="flex items-center max-w-xs text-sm text-white rounded-full focus:outline-none focus:shadow-solid"
+                                  id="user-menu"
+                                  aria-label="User menu"
+                                  aria-haspopup="true"
+                                  onClick={() => setIsOpen(!isOpen)}
+                                >
+                                  {profile?.image?.[0]?.contentUrl?.['/'] && (
+                                    <img
+                                      className="inline-block w-8 h-8 rounded"
+                                      src={`https://ipfs.infura.io/ipfs/${profile.image[0].contentUrl['/']}`}
+                                      alt="profile"
+                                    />
+                                  )}
+                                </button>
                               </div>
-                            </Transition>
-                          </div>
-                        </>
-                      )}
-                    </div>
-                  </div>
-                  <div className="flex -mr-2 md:hidden">
-                    <button
-                      className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
-                      onClick={() => setIsOpen(!isOpen)}
-                    >
-                      <MenuOpen className={`${isOpen ? 'hidden' : 'block'} w-6 h-6`} />
-                      <MenuClose className={`${isOpen ? 'block' : 'hidden'} w-6 h-6`} />
-                    </button>
-                  </div>
+                              <Transition
+                                show={isOpen}
+                                enter="transition ease-out duration-100 transform"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="transition ease-in duration-75 transform"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                              >
+                                <div className="absolute right-0 w-48 mt-2 origin-top-right rounded-md shadow-lg">
+                                  <div className="py-1 bg-white rounded-md shadow-xs">
+                                    <a
+                                      href="#"
+                                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    >
+                                      Settings
+                                    </a>
+                                    <a
+                                      href="#"
+                                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                    >
+                                      Sign out
+                                    </a>
+                                  </div>
+                                </div>
+                              </Transition>
+                            </div>
+                          </>
+                        </div>
+                      </div>
+                      <div className="flex -mr-2 md:hidden">
+                        <button
+                          className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+                          onClick={() => setIsOpen(!isOpen)}
+                        >
+                          <MenuOpen className={`${isOpen ? 'hidden' : 'block'} w-6 h-6`} />
+                          <MenuClose className={`${isOpen ? 'block' : 'hidden'} w-6 h-6`} />
+                        </button>
+                      </div>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
 
-            <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
-              <div className={`${isConnected ? 'block' : 'hidden'} px-2 py-3 sm:px-3`}>
+            <div className={`${isOpen && isConnected ? 'block' : 'hidden'} md:hidden`}>
+              <div className={`px-2 py-3 sm:px-3`}>
                 <Link
                   to="/contacts"
                   className={`${isLinkActive(
@@ -199,7 +199,7 @@ export default function App() {
                 </Link>
               </div>
               <div className="px-6 pt-4 pb-3 border-t border-gray-700">
-                {!isConnected ? <Connect /> : <Status />}
+                <Status />
               </div>
             </div>
           </nav>
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,23 +1,28 @@
 import * as React from 'react'
 import { useEffect } from 'react'
+import Connect from './Connect'
 import store from '../state/store'
 
 const Welcome: React.FC = (): JSX.Element => {
   const { setPageTitle } = store.usePageTitle
+  const { isConnected } = store.useStatus
 
   useEffect(() => {
     setPageTitle('Welcome')
   })
 
   return (
-    <div className="text-gray-300">
-      <div>Drasil Contacts</div>
-      <div>Control your information.</div>
-      <div>Never update your contacts again.</div>
-      <div>Your contacts keep their info updated for you.</div>
-      <div>Future-proof, portable, data.</div>
-      <div>Auto-updates and empowers users.</div>
-      <div>Secure and privacy respecting.</div>
+    <div className="px-6 sm:px-0 mt-4">
+      {!isConnected && <Connect />}
+      <div className="text-gray-300">
+        <div>Drasil Contacts</div>
+        <div>Control your information.</div>
+        <div>Never update your contacts again.</div>
+        <div>Your contacts keep their info updated for you.</div>
+        <div>Future-proof, portable, data.</div>
+        <div>Auto-updates and empowers users.</div>
+        <div>Secure and privacy respecting.</div>
+      </div>
     </div>
   )
 }
